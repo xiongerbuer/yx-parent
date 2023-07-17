83522226ada3cb4d6d2cf0e3f8e2a273259e0656
xiongyou <xiongyou@isiiot.com>
2023-07-17 11:41:28 +0800

    commit:依赖的包名修改

--- service-client/service-cart-client/src/main/java/com/atguigu/ssyx/cart/client/CartFeignClient.java
+++ service-client/service-cart-client/src/main/java/com/atguigu/ssyx/cart/client/CartFeignClient.java
@@ -1,6 +1,7 @@
 package com.atguigu.ssyx.cart.client;
 
 import com.atguigu.ssyx.model.order.CartInfo;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ public interface CartFeignClient {
      * @param userId
      * @return
      */
+    @ApiOperation(value = "根据用户Id 查询购物车列表")
     @GetMapping("/api/cart/inner/getCartCheckedList/{userId}")
     public List<CartInfo> getCartCheckedList(@PathVariable("userId") Long userId);
 }
